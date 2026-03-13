1748284b3f0ddf262a5a62905de3f7dc0155a129
HKDuy23114 <huakhanhduy12tn4@gmail.com>
2026-03-13 10:25:09 +0700

    config fb

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,21 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { getAuth, signInAnonymously } from "firebase/auth";
+
+// login firebase
+const auth = getAuth();
+
+signInAnonymously(auth)
+    .then(() => {
+        console.log("Firebase anonymous login success");
+    })
+    .catch((error) => {
+        console.error(error);
+    });
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <App />
+    </StrictMode>,
 )
